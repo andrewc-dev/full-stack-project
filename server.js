b7b3dae8dd4cfa01d3cc8e6a86f1b3727637bb08
Andrew Cai <andrew@yugabyte.com>
2020-04-20 15:15:56 -0700

    Add skeleton to project for api endpoints and React components

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser')
+var cors = require('cors')
+app.use(bodyParser.json());
+const port = 8000;
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+    db.run("CREATE TABLE TodoItems (name TEXT, completed BOOLEAN)");
+   
+    var stmt = db.prepare("INSERT INTO TodoItems VALUES (?, ?)");
+    stmt.run("Make a full-stack project", false);
+    
+    stmt.finalize();
+    db.each("SELECT rowid AS id, name, completed FROM TodoItems", function(err, row) {
+        console.log(row.id + ": " + row.name + " => " + row.completed);
+    });
+});
+
+app.options('*', cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/todos', (req, res) => {
+    let rows = [];
+    db.each("SELECT rowid AS id, name, completed FROM TodoItems", function(err, row) {
+        rows.push(row);
+    }, function () {
+        res.json(rows);
+    });
+})
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/todos', {
+      method: 'GET'
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      setTodos(data)
+    });
+  }, []);
+
+  const handleCheckbox = (event) => {
+    const newList = [...todos];    
+    const index = event.target.dataset["index"];
+    newList[index] = {
+      ...newList[index],
+      completed: event.target.checked
+    };
+    setTodos(newList);
+    
+    /* TODO: Add REST API call to persist change to DB */
+  }
+
+  const addNewItem = (e) => {
+    if(e.key === 'Enter'){
+      /* TODO: Add REST API call to submit new item */
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        To-Do List
       </header>
+      <div className="container">
+        <ul>
+          {todos.map((item, index) => (
+            <li key={`todo-${index}`}>
+              <input data-index={index} type="checkbox" checked={item.completed} onChange={handleCheckbox}/>
+              <span>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+        <input type="text" onKeyPress={addNewItem} placeholder="Add new task"/>
+      </div>
     </div>
   );
 }
